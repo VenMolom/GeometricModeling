7e0af4623965a9e08a8e8478b41d4a2d35d0521d
VenMolom <venmolom@protonmail.com>
2022-04-21 23:24:27 +0200

    fix tesselation amount calculation

--- Shader/hsBrezier.hlsl
+++ Shader/hsBrezier.hlsl
@@ -10,8 +10,13 @@ HSBrezierConstOutput CalcHSPatchConstants(
 {
 	HSBrezierConstOutput output;
 
-	float4 maxx = max(ip[0].pos / ip[0].pos.w, max(ip[1].pos, max(ip[2].pos, ip[3].pos)));
-	float4 minn = min(ip[0].pos, min(ip[1].pos, min(ip[2].pos, ip[3].pos)));
+	float4 p1 = ip[0].pos / ip[0].pos.w;
+	float4 p2 = ip[1].pos / ip[1].pos.w;
+	float4 p3 = ip[2].pos / ip[2].pos.w;
+	float4 p4 = ip[3].pos / ip[3].pos.w;
+
+	float4 maxx = max(p1, max(p2, max(p3, p4)));
+	float4 minn = min(p1, min(p2, min(p3, p4)));
 
     float2 start = clamp(minn.xy, float2(-1, -1), float2(1, 1));
     float2 end = clamp(maxx.xy, float2(-1, -1), float2(1, 1));
@@ -21,12 +26,10 @@ HSBrezierConstOutput CalcHSPatchConstants(
     float2 startViewport = mad(scale, start, scale);
     float2 endViewport = mad(scale, end, scale);
 
-    //float tesselationAmount = clamp(ceil(distance(startViewport, endViewport) / 64.0f), 1.0f, 64.0f);
-    float tesselationAmount = clamp(ceil(distance(startViewport, endViewport)), 1.0f, 64.0f);
+    float tesselationAmount = clamp(ceil(distance(startViewport, endViewport) / 64.0f), 1.0f, 64.0f);
 
-    //output.edges[0] = tesselationAmount;
-    output.edges[0] = 1.0f;
-    output.edges[1] = tesselationAmount;
+    output.edges[0] = tesselationAmount;
+    output.edges[1] = 64.0f;
     output.patchSize = PatchID == tesselationSetting.z ? tesselationSetting.w : 4;
 
 	return output;
