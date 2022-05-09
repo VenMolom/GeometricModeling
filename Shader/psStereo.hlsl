5f2fce2b5c19432c08bef3318a82c0d37c8f2350
Molom <venmolom@protonmail.com>
2022-05-09 14:33:49 +0200

    normalized blending

--- Shader/psStereo.hlsl
+++ Shader/psStereo.hlsl
@@ -13,5 +13,13 @@ float4 main(PSStereoIn i) : SV_TARGET
 {
     float3 left = leftEye.Sample(colorSampler, i.tex).rgb;
     float3 right = rightEye.Sample(colorSampler, i.tex).rgb;
-    return float4(leftColor * left + rightColor * right, 1.f);
+
+    float2 r = normalize(float2(leftColor.r, rightColor.r));
+    float2 g = normalize(float2(leftColor.g, rightColor.g));
+    float2 b = normalize(float2(leftColor.b, rightColor.b));
+
+    float3 leftFilter = float3(r.x, g.x, b.x);
+    float3 rightFilter = float3(r.y, g.y, b.y);
+
+    return float4(leftFilter * left + rightFilter * right, 1.f);
 }
